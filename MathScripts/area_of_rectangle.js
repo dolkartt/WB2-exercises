4abb1f04cd27fc035e73030377e41f7f52d442cc
Dolkart <130485539+dolkartt@users.noreply.github.com>
2023-04-25 09:47:07 -0400

    second commit

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,9 @@
+
+var lengthOfRectangle = 8;
+var widthOfRectangle = 2;
+
+// Calculations
+var areaOfRectangle = lengthOfRectangle * widthOfRectangle;
+
+//Outputs
+console.log("The area of a rectangle is " + areaOfRectangle);
